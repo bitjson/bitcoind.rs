f1a5e3c5916547ff202a34f963c068c84aadf427
Nicholas Alan Cardin <nacardin@gmail.com>
2015-12-20 12:29:23 -0500

    leveldb test

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,57 @@
 extern crate bitcoin;
+extern crate leveldb;
 
 pub mod node;
 
 use node::peer::connect;
 use std::io;
 use std::io::prelude::*;
+use std::path::Path;
+
+use leveldb::database::Database;
+use leveldb::iterator::Iterable;
+use leveldb::options::{Options,WriteOptions,ReadOptions};
+use leveldb::database::kv::KV;
 
 fn main() {
-    connect("127.0.0.1", 8333);
+    test_ldb();
+
+    //connect("127.0.0.1", 8333);
 
     let stdin = io::stdin();
     for line in stdin.lock().lines() {
         println!("{}", line.unwrap());
     }
+}
+
+
+fn test_ldb() {
+
+  let mut options = Options::new();
+  options.create_if_missing = true;
+  let mut database = match Database::open(Path::new("./data"), options) {
+      Ok(db) => { db },
+      Err(e) => { panic!("failed to open database: {:?}", e) }
+  };
+
+  let write_opts = WriteOptions::new();
+  match database.put(write_opts, 1, ("hello".to_string().as_bytes())) {
+      Ok(_) => { () },
+      Err(e) => { panic!("failed to write to database: {:?}", e) }
+  };
+
+  let read_opts = ReadOptions::new();
+  let res = database.get(read_opts,  1);
+  match res {
+    Ok(data) => {
+        println!("{:?}", data);
+    //   assert!(data.is_some());
+    //   assert_eq!(data, Some(vec![1]));
+    }
+    Err(e) => { panic!("failed reading data: {:?}", e) }
+  }
+
+  let read_opts = ReadOptions::new();
+  let mut iter = database.iter(read_opts);
+  let entry = iter.next();
 }
