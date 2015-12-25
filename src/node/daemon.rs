ca077347323f15f498f5b364525b377104f48c77
Nicholas Alan Cardin <nacardin@gmail.com>
2015-12-24 23:36:19 -0500

    pipe inv

--- src/node/daemon.rs
+++ src/node/daemon.rs
@@ -3,6 +3,7 @@ use node::peer::connect;
 
 use std::thread;
 use std::time::Duration;
+use std::sync::mpsc::{channel, Sender, Receiver};
 
 use bitcoin::util::hash::Sha256dHash;
 
@@ -12,6 +13,7 @@ use bitcoin::network::message::NetworkMessage;
 use bitcoin::network::message::SocketResponse;
 use bitcoin::network::message_network::VersionMessage;
 use bitcoin::network::message_blockdata::GetBlocksMessage;
+use bitcoin::network::message_blockdata::Inventory;
 
 extern crate rustc_serialize;
 
@@ -20,18 +22,43 @@ use self::rustc_serialize::hex::FromHex;
 pub fn start() {
     test_ldb();
 
-    let mut socket = connect("127.0.0.1", 8333);
+
+    let (txDaemon, rxDaemon) = channel::<Vec<Inventory>>();
+
+    let mut socket = connect("127.0.0.1", 8333, txDaemon);
+
+
+    thread::spawn( move || {
+        loop {
+            match rxDaemon.recv() {
+                Ok(payload) => {
+                    println!("recv inv thru tunnl {:?}", payload.len());
+
+                    for inv in payload {
+                        println!("inv iter {:?}", inv);
+                    }
+                },
+                Err(e) => {
+                    println!("getblocks error {:?}",e);
+                }
+            }
+        }
+    });
+
+
 
     let waitTime = Duration::from_secs(10);
 
 
-let hash = "00000000006bdf3d9399d565df7571eb773986babca04872d5048a838d5b14f9".from_hex().unwrap();
+    let hash = "00000000d14451e1f057e65b076f3aacb30da7cf0fde3d43419b08a207b94bfd".from_hex().unwrap();
+    let hash2 = "00000000c2d671c26e69d1baede1e4e6891a63dca35457d58175d55f98720d6c".from_hex().unwrap();
 
-    let sha = Sha256dHash::from_data(&hash);
+        let sha = Sha256dHash::from_data(&hash);
+            let sha2 = Sha256dHash::from_data(&hash2);
 
     let mut locator_hashes : Vec<Sha256dHash> = Vec::new();
     locator_hashes.push(sha);
-    let stop_hash = sha;
+    let stop_hash = sha2;
 
     let a = GetBlocksMessage::new(locator_hashes, stop_hash);
 
--- src/node/peer.rs
+++ src/node/peer.rs
@@ -13,218 +13,220 @@ use bitcoin::network::constants::Network;
 use bitcoin::network::message::NetworkMessage;
 use bitcoin::network::message::SocketResponse;
 use bitcoin::network::message_network::VersionMessage;
+use bitcoin::network::message_blockdata::Inventory;
 
-pub fn connect(host: &'static str, port: u16) -> Socket{
+pub fn connect(host: &'static str, port: u16, txDaemon: Sender<Vec<Inventory>>) -> Socket{
 
 
 
     let mut socket = Socket::new(Network::Testnet);
 
     match socket.connect(host, port) {
-        Ok(()) => recv_loop(socket.clone()),
+        Ok(()) => recv_loop(socket.clone(), txDaemon),
         Err(e) => {
             println!("error {:?}", e);
         }
     }
 
-    fn recv_loop(socket : Socket) {
+    fn send_ping(tx: Sender<NetworkMessage>) {
 
-    thread::spawn( move || {
+        let ping_interval = Duration::from_secs(10);
+        thread::sleep(ping_interval);
 
-        fn send_ping(tx: Sender<NetworkMessage>) {
+        let mut rng = rand::thread_rng();
+        let between = Range::new(u64::MIN, u64::MAX);
+        let nonce = between.ind_sample(&mut rng);
 
-            let ping_interval = Duration::from_secs(10);
+        let ping_message = NetworkMessage::Ping(nonce);
+        println!("sending ping");
+
+        match tx.send(ping_message) {
+            Ok(()) => {
+                println!("ping sent with nonce {:?}", nonce);
+            },
+            Err(e) => {
+                println!("error {:?}",e);
+            }
+        }
+    }
+
+    fn send_pong(socket: &mut Socket, nonce: u64) {
+        let pong_message = NetworkMessage::Pong(nonce);
+
+        match socket.send_message(pong_message) {
+            Ok(()) => {
+                println!("pong sent");
+            },
+            Err(e) => {
+                println!("error {:?}",e);
+            }
+        }
+    }
+
+    fn sender(mut socket: Socket, rx: Receiver<NetworkMessage>) {
+        loop {
+            let message = rx.recv().unwrap();
+            match socket.send_message(message) {
+                Ok(()) => {
+                    println!("sent");
+                },
+                Err(e) => {
+                    println!("error {:?}",e);
+                }
+            }
+        }
+    }
+
+
+    fn pinger(mut socket: Socket, rx: Receiver<NetworkMessage>) {
+
+        let ping_interval = Duration::from_secs(120);
+        let mut rng = rand::thread_rng();
+        let between = Range::new(u64::MIN, u64::MAX);
+
+        loop {
+
+            let nonce = between.ind_sample(&mut rng);
+            let ping_message = NetworkMessage::Ping(nonce);
+
+            match socket.send_message(ping_message) {
+                Ok(()) => {
+                    println!("sent ping ");
+                },
+                Err(e) => {
+                    println!("error {:?}",e);
+                }
+            }
+
+
             thread::sleep(ping_interval);
 
-            let mut rng = rand::thread_rng();
-            let between = Range::new(u64::MIN, u64::MAX);
-            let nonce = between.ind_sample(&mut rng);
-
-            let ping_message = NetworkMessage::Ping(nonce);
-            println!("sending ping");
-
-            match tx.send(ping_message) {
-                Ok(()) => {
-                    println!("ping sent with nonce {:?}", nonce);
+            match rx.try_recv() {
+                Ok(payload) => {
+                    println!("pong received by pinger {:?} ", payload);
                 },
                 Err(e) => {
                     println!("error {:?}",e);
+                    break;
                 }
             }
+
+
         }
 
-        fn send_pong(socket: &mut Socket, nonce: u64) {
-            let pong_message = NetworkMessage::Pong(nonce);
+        println!("Peer didn't respond to ping");
+    }
 
-            match socket.send_message(pong_message) {
-                Ok(()) => {
-                    println!("pong sent");
-                },
-                Err(e) => {
-                    println!("error {:?}",e);
-                }
-            }
-        }
+    fn receiver(mut socket: Socket, tx: Sender<NetworkMessage>, txDaemon: Sender<Vec<Inventory>>) {
+        loop {
+            match socket.receive_message() {
+                Ok(payload) => {
+                    // println!("received {:?}",payload);
 
-        fn sender(mut socket: Socket, rx: Receiver<NetworkMessage>) {
-            loop {
-                let message = rx.recv().unwrap();
-                match socket.send_message(message) {
-                    Ok(()) => {
-                        println!("sent");
-                    },
-                    Err(e) => {
-                        println!("error {:?}",e);
-                    }
-                }
-            }
-        }
-
-
-        fn pinger(mut socket: Socket, rx: Receiver<NetworkMessage>) {
-
-            let ping_interval = Duration::from_secs(120);
-            let mut rng = rand::thread_rng();
-            let between = Range::new(u64::MIN, u64::MAX);
-
-            loop {
-
-                let nonce = between.ind_sample(&mut rng);
-                let ping_message = NetworkMessage::Ping(nonce);
-
-                match socket.send_message(ping_message) {
-                    Ok(()) => {
-                        println!("sent ping ");
-                    },
-                    Err(e) => {
-                        println!("error {:?}",e);
-                    }
-                }
-
-
-                thread::sleep(ping_interval);
-
-                match rx.try_recv() {
-                    Ok(payload) => {
-                        println!("pong received by pinger {:?} ", payload);
-                    },
-                    Err(e) => {
-                        println!("error {:?}",e);
-                        break;
-                    }
-                }
-
-
-            }
-
-            println!("Peer didn't respond to ping");
-        }
-
-        fn receiver(mut socket: Socket, tx: Sender<NetworkMessage>) {
-            loop {
-                match socket.receive_message() {
-                    Ok(payload) => {
-                        println!("received {:?}",payload);
-                        //tx.send(payload.clone());
-                        match payload {
-                            NetworkMessage::Version(nonce) => {
-                                println!("ping nonce {:?}", nonce);
-                            }
-                            NetworkMessage::Verack => {
-                                println!("verack");
-                            }
-                            NetworkMessage::Ping(nonce) => {
-                                println!("ping nonce {:?}", nonce);
-                                send_pong(&mut socket, nonce);
-                            }
-                            NetworkMessage::Addr(addr) => {
-                                println!("addr {:?}", addr);
-                            }
-                            NetworkMessage::Inv(inv) => {
-                                println!("Inv {:?}", inv);
-                            }
-                            NetworkMessage::GetData(inv) => {
-                                println!("GetData {:?}", inv);
-                            }
-                            NetworkMessage::NotFound(inv) => {
-                                println!("NotFound {:?}", inv);
-                            }
-                            NetworkMessage::GetBlocks(inv) => {
-                                println!("GetBlocks {:?}", inv);
-                            }
-                            NetworkMessage::GetHeaders(inv) => {
-                                println!("GetHeaders {:?}", inv);
-                            }
-                            NetworkMessage::Tx(inv) => {
-                                println!("Tx {:?}", inv);
-                            }
-                            NetworkMessage::Block(inv) => {
-                                println!("Block {:?}", inv);
-                            }
-                            NetworkMessage::Headers(inv) => {
-                                println!("Headers {:?}", inv);
-                            }
-                            NetworkMessage::Pong(inv) => {
-                                println!("Pong {:?}", inv);
-                                tx.send(payload);
-                            }
-                            NetworkMessage::MemPool => {
-                                println!("MemPool");
-                            }
+                    let message : NetworkMessage = payload;
+                    match message {
+                        NetworkMessage::Version(nonce) => {
+                            println!("ping nonce {:?}", nonce);
+                        }
+                        NetworkMessage::Verack => {
+                            println!("verack");
+                        }
+                        NetworkMessage::Ping(nonce) => {
+                            println!("ping nonce {:?}", nonce);
+                            send_pong(&mut socket, nonce);
+                        }
+                        NetworkMessage::Addr(addr) => {
+                            println!("addr {:?}", addr);
+                        }
+                        NetworkMessage::Inv(inv) => {
+                            txDaemon.send(inv);
+                        }
+                        NetworkMessage::GetData(inv) => {
+                            println!("GetData {:?}", inv);
+                        }
+                        NetworkMessage::NotFound(inv) => {
+                            println!("NotFound {:?}", inv);
+                        }
+                        NetworkMessage::GetBlocks(inv) => {
+                            println!("GetBlocks {:?}", inv);
+                        }
+                        NetworkMessage::GetHeaders(inv) => {
+                            println!("GetHeaders {:?}", inv);
+                        }
+                        NetworkMessage::Tx(inv) => {
+                            println!("Tx {:?}", inv);
+                        }
+                        NetworkMessage::Block(inv) => {
+                            println!("Block {:?}", inv);
+                        }
+                        NetworkMessage::Headers(inv) => {
+                            println!("Headers {:?}", inv);
+                        }
+                        NetworkMessage::Pong(inv) => {
+                            println!("Pong {:?}", inv);
+                            tx.send(message);
+                        }
+                        NetworkMessage::MemPool => {
+                            println!("MemPool");
                         }
                     }
-                    Err(e) => {
-                        println!("error {:?}",e);
-                    }
-                }
-
-                let ping_interval = Duration::from_secs(1);
-                thread::sleep(ping_interval);
-            }
-        }
-
-        fn send_version_message(mut socket: Socket, version_message: VersionMessage) {
-            let network_message = NetworkMessage::Version(version_message);
-            match socket.send_message(network_message) {
-                Ok(()) => {
-                    let (txMessageReceiver, rxMessageReceiver) = channel::<NetworkMessage>();
-                    // let (txMessageSender, rxMessageSender) = channel::<NetworkMessage>();
-
-                    let sender_socket = socket.clone();
-                    let receiver_socket = socket.clone();
-
-                    thread::spawn( move || {
-                        receiver(receiver_socket, txMessageReceiver);
-                    });
-
-                    thread::spawn( move || {
-                        pinger(sender_socket, rxMessageReceiver);
-                    });
-
-                    // thread::spawn( move || {
-                    //     loop {
-                    //         send_pong(&mut sender_socket.clone(), 2353);
-                    //     }
-                    // });
-
                 }
                 Err(e) => {
                     println!("error {:?}",e);
                 }
             }
+
+            let ping_interval = Duration::from_secs(1);
+            thread::sleep(ping_interval);
         }
+    }
 
-            match VersionMessage::new(14213, socket.clone(), 12421, 2048) {
-                Ok(version_message) => send_version_message(socket, version_message),
-                Err(e) => {
-                    println!("error {:?}", e);
-                }
+    fn send_version_message(mut socket: Socket, version_message: VersionMessage, txDaemon: Sender<Vec<Inventory>>) {
+        let network_message = NetworkMessage::Version(version_message);
+        match socket.send_message(network_message) {
+            Ok(()) => {
+                let (txMessageReceiver, rxMessageReceiver) = channel::<NetworkMessage>();
+                // let (txMessageSender, rxMessageSender) = channel::<NetworkMessage>();
+
+                let sender_socket = socket.clone();
+                let receiver_socket = socket.clone();
+
+                thread::spawn( move || {
+                    receiver(receiver_socket, txMessageReceiver, txDaemon);
+                });
+
+                thread::spawn( move || {
+                    pinger(sender_socket, rxMessageReceiver);
+                });
+
+                // thread::spawn( move || {
+                //     loop {txDaemon: Sender<Vec<Inventory>>
+                //         send_pong(&mut sender_socket.clone(), 2353);
+                //     }
+                // });
+
             }
+            Err(e) => {
+                println!("error {:?}",e);
+            }
+        }
+    }
+
+    fn recv_loop(socket : Socket, txDaemon: Sender<Vec<Inventory>>) {
+
+        thread::spawn( move || {
+
+                match VersionMessage::new(14213, socket.clone(), 12421, 2048) {
+                    Ok(version_message) => send_version_message(socket, version_message, txDaemon),
+                    Err(e) => {
+                        println!("error {:?}", e);
+                    }
+                }
 
 
-    });
+        });
 
-}
+    }
 
     return socket;
 
